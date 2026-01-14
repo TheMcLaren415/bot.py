46c3b62324085f76ce109d7e801435a523d6c729
TheMcLaren415 <robloxofficial400@gmail.com>
2026-01-14 20:27:26 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,16 +1,18 @@
-import os
 import discord
 from discord.ext import commands
 
 TOKEN = os.getenv("DISCORD_TOKEN")
+GUILD_ID = 1395001990542528573
 
 intents = discord.Intents.default()
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
-    await bot.tree.clear_commands()
-    await bot.tree.sync()
-    print("Slash-команды удалены")
+    guild = discord.Object(id=GUILD_ID)
+    bot.tree.clear_commands(guild=guild)
+    await bot.tree.sync(guild=guild)
+    print("Готово. Команды удалены.")
+    await bot.close()
 
 bot.run(TOKEN)
